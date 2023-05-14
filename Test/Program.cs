2fb6356633c7ef271ba59a611b874a6146216258
Andy P <apokk@tktk.ee>
2023-05-14 19:32:38 +0300

    add week8 code

---
+++ Test/Program.cs
@@ -0,0 +1,8 @@
+﻿string path = @"C:\Users\Desktop\Data\";
+string fileName = "chuck.txt";
+string[] lines = File.ReadAllLines($"{path} + {fileName}");
+
+foreach (string hero in lines)
+{
+    Console.WriteLine(hero);
+}
